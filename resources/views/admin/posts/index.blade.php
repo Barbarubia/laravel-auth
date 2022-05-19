a15576732200e28237797d9bc21a55cae220b731
Barbarubia <dnl.desimone@gmail.com>
2022-05-19 08:59:54 +0200

    creato index dei posts e reindirizzamenti

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('pageTitle', 'Admin Control Panel')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Admin Control Panel</h1>
+            </div>
+        </div>
+    </div>
+
+@endsection
