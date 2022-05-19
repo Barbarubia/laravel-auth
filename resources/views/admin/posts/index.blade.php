6f4cd4b177d04b132c50f84df8f84e11c82f0649
Barbarubia <dnl.desimone@gmail.com>
2022-05-19 15:02:32 +0200

    modificato layout index admin/posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -15,53 +15,57 @@
                 <a href="{{ route('admin.posts.create') }}" class="btn btn-primary">Add a new post</a>
             </div>
         </div>
-        <div class="row bg-success">
-            <div class="col-1 border-right">
-                <h6>#id</h6>
+        <div class="row bg-dark text-white">
+            <div class="col-1 d-flex align-items-center border py-1">
+                <h5 class="my-0">#id</h5>
             </div>
-            <div class="col-3 border-right">
-                <h6>Title</h6>
+            <div class="col-3 d-flex align-items-center border py-1">
+                <h5 class="my-0">Title</h5>
             </div>
-            <div class="col-3 border-right">
-                <h6>Slug</h6>
+            <div class="col-3 d-flex align-items-center border py-1">
+                <h5 class="my-0">Slug</h5>
             </div>
-            <div class="col-1 border-right">
-                <h6>Created at</h6>
+            <div class="col-1 d-flex align-items-center border py-1">
+                <h5 class="my-0">Created at</h5>
             </div>
-            <div class="col-1 border-right">
-                <h6>Updated at</h6>
+            <div class="col-1 d-flex align-items-center border py-1">
+                <h5 class="my-0">Updated at</h5>
             </div>
-            <div class="col-3">
-                <h6>Options</h6>
+            <div class="col-3 d-flex align-items-center border py-1">
+                <h5 class="my-0">Options</h5>
             </div>
         </div>
 
         @foreach ($posts as $post)
-            <div class="row bg-info">
-                <div class="col-1 border-right">
-                    <p>{{ $post->id }}</p>
+            <div class="row bg-secondary">
+                <div class="col-1 d-flex align-items-center border py-1">
+                    <p class="my-0">{{ $post->id }}</p>
                 </div>
-                <div class="col-3 border-right">
-                    <p>{{ $post->title }}</p>
+                <div class="col-3 d-flex align-items-center border py-1">
+                    <p class="my-0">{{ $post->title }}</p>
                 </div>
-                <div class="col-3 border-right">
-                    <p>{{ $post->slug }}</p>
+                <div class="col-3 d-flex align-items-center border py-1">
+                    <p class="my-0">{{ $post->slug }}</p>
                 </div>
-                <div class="col-1 border-right">
-                    <p>{{ date('d/m/Y', strtotime($post->created_at)) }}</p>
+                <div class="col-1 d-flex align-items-center border py-1">
+                    <p class="my-0">{{ date('d/m/Y', strtotime($post->created_at)) }}</p>
                 </div>
-                <div class="col-1 border-right">
-                    <p>{{ date('d/m/Y', strtotime($post->updated_at)) }}</p>
+                <div class="col-1 d-flex align-items-center border py-1">
+                    <p class="my-0">{{ date('d/m/Y', strtotime($post->updated_at)) }}</p>
                 </div>
-                <div class="col-3">
-                    <a class="btn btn-primary" href="{{ route('admin.posts.show', $post->slug) }}"><i class="fa-solid fa-eye"></i></a>
-                    <a class="btn btn-primary" href="{{ route('admin.posts.edit', $post->slug) }}"><i class="fa-solid fa-pen-to-square"></i></a>
-                    <button class="btn btn-danger btn-delete" data-id="{{ $post->id }}"><i class="fa-solid fa-trash-can"></i></button>
+                <div class="col-3 d-flex flex-wrap justify-content-between align-items-center border py-1">
+                    <a class="btn btn-primary" href="{{ route('admin.posts.show', $post->slug) }}"><i class="fa-solid fa-eye"></i> View</a>
+                    <a class="btn btn-primary" href="{{ route('admin.posts.edit', $post->slug) }}"><i class="fa-solid fa-pen-to-square"></i> Edit</a>
+                    <button class="btn btn-danger btn-delete" data-id="{{ $post->id }}"><i class="fa-solid fa-trash-can"></i> Delete</button>
                 </div>
             </div>
         @endforeach
 
-        {{ $posts->links() }}
+        <div class="row mt-3">
+            <div class="col d-flex justify-content-center">
+                {{ $posts->links() }}
+            </div>
+        </div>
 
     </div>
 
